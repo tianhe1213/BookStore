abbf01dfd084dfb3d7920cea62350fb419105dec
苏俊杰 <742953234@qq.com>
2021-02-15 11:42:23 +0800

    修改了5行代码

--- src/com/atguigu/bookstore/entities/Junjie.java
+++ src/com/atguigu/bookstore/entities/Junjie.java
@@ -3,6 +3,6 @@ package com.atguigu.bookstore.entities;
 public class Junjie {
 
     public static void main(String[] args) {
-        System.out.println("1111111");
+        System.out.println("111111123");
     }
 }
